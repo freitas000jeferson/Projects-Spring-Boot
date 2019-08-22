970d67afb9a2a2a627f1a454857853c0cb8239ff
Jeferson Gonçalves de Freitas <freitas000jeferson@gmail.com>
2019-08-22 16:19:11 -0300

    inicio de projeto

---
+++ projeto01-workshop/src/main/java/com/freitas000jeferson/projeto01workshop/Projeto01WorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.freitas000jeferson.projeto01workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto01WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto01WorkshopApplication.class, args);
+	}
+
+}
